68f66536d97eb1cc7465a5e9a4b92e93ff1892d5
Andrew Liebchen <andrewliebchen@gmail.com>
2018-11-22 12:16:21 -0800

    Simplify style

--- src/FeedItem.js
+++ src/FeedItem.js
@@ -13,15 +13,8 @@ import Text from "./Text";
 import Video from "./Video";
 import { themeGet } from "styled-system";
 
-const Root = styled(Box)`
-  background-color: ${themeGet("colors.gray0")};
-  overflow: visible;
-`;
-
-const Card = styled(Flex)`
+const Root = styled(Flex)`
   background-color: white;
-  border-radius: 8px;
-  box-shadow: 0 2px 10px 1px rgba(0, 0, 0, 0.05);
 `;
 
 const Feature = styled.div`
@@ -46,21 +39,19 @@ const FeatureItem = type => {
 };
 
 const FeedItem = props => (
-  <Root px={2} py={1}>
-    <Card column>
-      <Flex align="center" p={2}>
-        <Avatar />
-        <Box pl={1} w={1} pt={1}>
-          <Text lines={1} size={1} />
-          <Text lines={1} size={2} />
-        </Box>
-      </Flex>
-      <Feature>{FeatureItem(props.type)}</Feature>
-      <Box p={2}>
-        <Text pb={1} />
-        <LikeButton />
+  <Root column mb={2}>
+    <Flex align="center" p={2}>
+      <Avatar />
+      <Box pl={1} w={1} pt={1}>
+        <Text lines={1} size={1} />
+        <Text lines={1} size={2} />
       </Box>
-    </Card>
+    </Flex>
+    <Feature>{FeatureItem(props.type)}</Feature>
+    <Box p={2}>
+      <Text pb={1} />
+      <LikeButton />
+    </Box>
   </Root>
 );
 
