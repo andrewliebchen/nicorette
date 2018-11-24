76b7c43f7cb4cce0932facaf9687cc109267517e
Andrew Liebchen <andrewliebchen@gmail.com>
2018-11-23 16:17:30 -0800

    Animate like button

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -1,5 +1,31 @@
 import React, { Component } from "react";
 import theme from "./theme";
+import styled, { keyframes } from "styled-components";
+import { themeGet } from "styled-system";
+
+const explode = keyframes`
+  0% {
+    transform:  scale(1);
+  }
+
+  80% {
+    transform:  scale(1.5);
+  }
+
+  100% {
+    transform:  scale(1);
+  }
+`;
+
+const Root = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const Heart = styled.svg`
+  animation: ${explode} 0.2s ease-out forwards;
+  animation-play-state: ${props => (props.liked ? "running" : "paused")};
+`;
 
 class LikeButton extends Component {
   constructor(props) {
@@ -12,8 +38,8 @@ class LikeButton extends Component {
   render() {
     const { liked } = this.state;
     return (
-      <div onClick={() => this.setState({ liked: !liked })}>
-        <svg height={30} viewBox="0 0 200 200">
+      <Root onClick={() => this.setState({ liked: !liked })}>
+        <Heart height={30} viewBox="0 0 200 200" liked={liked}>
           <path
             d="M139.523717,22.0031862 C124.115952,21.9516148 109.612241,29.2402941 100.5,41.6141226 C88.0657184,24.8385508 66.224375,17.9057468 46.3362799,24.4216612 C26.4481848,30.9375756 13.0054387,49.4304664 13,70.2816498 C13,129.602382 94.662875,175.46778 98.1396972,177.391064 C99.607639,178.202979 101.392361,178.202979 102.860303,177.391064 C106.337222,175.46778 188,129.602382 188,70.2816498 C187.970103,43.6305338 166.284035,22.0329616 139.523717,22.0031862 Z"
             fill={liked ? theme.colors.pink : "none"}
@@ -22,8 +48,8 @@ class LikeButton extends Component {
             strokeLinecap="round"
             strokeLinejoin="round"
           />
-        </svg>
-      </div>
+        </Heart>
+      </Root>
     );
   }
 }
