cf2be29dbf8105fb75447801f85a2a66988703ed
Andrew Liebchen <andrewliebchen@gmail.com>
2018-11-24 16:11:55 -0800

    Custom icon and logos

--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,7 @@ import { themeGet } from "styled-system";
 import React from "react";
 import styled from "styled-components";
 import Headroom from "react-headroom";
+import logo from "./images/n_filled.svg";
 
 const Root = styled(Flex)`
   background-color: ${themeGet("colors.gray9")};
@@ -13,7 +14,7 @@ const Root = styled(Flex)`
 const Header = props => (
   <Headroom>
     <Root px={2} w={1} justify="center" align="center">
-      <b>Nicorette</b>
+      <img src={logo} height={24} alt="Nicorette logo" />
     </Root>
   </Headroom>
 );
